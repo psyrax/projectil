a17cf1152a696e89d235316f47abe7675c7af132
Psyrax <psyrax@opiumgarden.org>
2019-06-08 13:00:07 -0500

    Initial Commit

---
+++ Proyectil/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Proyectil
+//
+//  Created by Jose Espinoza Cuevas on 6/8/19.
+//  Copyright © 2019 Jose Espinoza Cuevas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Proyectil/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Proyectil
+//
+//  Created by Jose Espinoza Cuevas on 6/8/19.
+//  Copyright © 2019 Jose Espinoza Cuevas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
